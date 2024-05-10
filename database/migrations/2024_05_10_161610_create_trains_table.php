826a5bd63b6673ecf151c35a750fccb6f1f91b7d
devitoangelo <angelodevito4@gmail.com>
2024-05-10 18:56:11 +0200

    create trains migrations function

--- database/migrations/2024_05_10_161610_create_trains_table.php
+++ database/migrations/2024_05_10_161610_create_trains_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('agency', 50)->nullable();
+            $table->string('Departure_station', 100)->nullable();
+            $table->string('Arrival_station', 100);
+            $table->dateTime('Departure_time');
+            $table->dateTime('Arrival_time');
+            $table->string('Train_code', 100);
+            $table->tinyInteger('Number_carriages',)->nullable();
+            $table->boolean('in_time',)->nullable()->default(1);
+            $table->boolean('Deleted',)->nullable()->default(0);
             $table->timestamps();
         });
     }
