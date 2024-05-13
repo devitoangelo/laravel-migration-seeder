8d3f91f8be6061501261a32674b5369c1af7eeec
devitoangelo <angelodevito4@gmail.com>
2024-05-13 13:31:57 +0200

    add array in file db.php

---
+++ config/db.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    'trains' => [
+        [
+
+            'agency' => 'Italia',
+            'Departure_station' => 'Taranto',
+            'Arrival_station' => 'Napoli',
+            'Departure_time' => '2024-11-10 17:29:06',
+            'Arrival_time' => '2024-11-10 19:11:02',
+            'Train_code' => 132,
+            'Number_carriages' => 4,
+            'in_time' => 1,
+            'Deleted' => 0
+        ],
+        [
+
+            'agency' => 'Italia',
+            'Departure_station' => 'Milano',
+            'Arrival_station' => 'Bari',
+            'Departure_time' => '2024-12-10 15:13:09',
+            'Arrival_time' => '2024-12-10 19:11:02',
+            'Train_code' => 133,
+            'Number_carriages' => 4,
+            'in_time' => 1,
+            'Deleted' => 0
+        ],
+        [
+
+            'agency' => 'Italia',
+            'Departure_station' => 'Matera',
+            'Arrival_station' => 'Sicilia',
+            'Departure_time' => '2024-11-10 7:12:03',
+            'Arrival_time' => '2024-11-10 9:11:02',
+            'Train_code' => 134,
+            'Number_carriages' => 5,
+            'in_time' => 1,
+            'Deleted' => 0
+        ],
+
+    ],
+
+];
