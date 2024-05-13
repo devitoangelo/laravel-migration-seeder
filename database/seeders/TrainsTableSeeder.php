6fe1f3acf02bb3708a4d71cf76ca407564407573
devitoangelo <angelodevito4@gmail.com>
2024-05-13 16:16:04 +0200

    add faker models and add the parameters faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,34 +2,56 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use App\Models\train;
 
 class TrainsTableSeeder extends Seeder
 {
-  
-    public function run(): void
+
+    public function run(Faker $faker): void
     {
-      
-        $trains = config('db.trains');
-
-        foreach ($trains as  $train) {
-            $newTrain = new train();
-            $newTrain->agency = $train['agency'];
-            $newTrain->Departure_station = $train['Departure_station'];
-            $newTrain->Arrival_station = $train['Arrival_station'];
-            $newTrain->Departure_time = $train['Departure_time'];
-            $newTrain->Arrival_time = $train['Arrival_time'];
-            $newTrain->Train_code = $train['Train_code'];
-            $newTrain->Number_carriages = $train['Number_carriages'];
-            $newTrain->in_time = $train['in_time'];
-            $newTrain->Deleted = $train['Deleted'];
-            $newTrain->save();
 
 
+        for ($i = 0; $i < 10; $i++) 
+        {
 
+            $train = new Train();
+            $train->agency = $faker->countryCode();
+            $train->Departure_station = $faker->countryCode();
+            $train->Arrival_station = $faker->countryCode();
+            $train->Departure_time = $faker->date() && $faker->time();
+            $train->Arrival_time = $faker->date() && $faker->time();
+            $train->Train_code = $faker->numberBetween(0, 999);
+            $train->Number_carriages = $faker->numberBetween(0, 999);
+            $train->in_time = $faker->boolean();
+            $train->Deleted = $faker->boolean();
+            $train->save();
         }
+
+
+
+
+
+        // Passaggio con Seeder
+        // ho passatto l'array in config/db.php
+        // $trains = config('db.trains');
+
+        // foreach ($trains as  $train) {
+        //     $newTrain = new train();
+        //     $newTrain->agency = $train['agency'];
+        //     $newTrain->Departure_station = $train['Departure_station'];
+        //     $newTrain->Arrival_station = $train['Arrival_station'];
+        //     $newTrain->Departure_time = $train['Departure_time'];
+        //     $newTrain->Arrival_time = $train['Arrival_time'];
+        //     $newTrain->Train_code = $train['Train_code'];
+        //     $newTrain->Number_carriages = $train['Number_carriages'];
+        //     $newTrain->in_time = $train['in_time'];
+        //     $newTrain->Deleted = $train['Deleted'];
+        //     $newTrain->save();
+
+
 
     }
 }
