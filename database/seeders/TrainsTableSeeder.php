9fc09a45c1cbb6dfbb6dc340b0500f0436448f86
devitoangelo <angelodevito4@gmail.com>
2024-05-13 13:24:49 +0200

    add new train in page to seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,7 +15,7 @@ class TrainsTableSeeder extends Seeder
         $trains = [
             [
 
-                'agency'=>'Fracia',
+                'agency'=>'Italia',
                 'Departure_station'=>'Taranto',
                 'Arrival_station'=>'Napoli',
                 'Departure_time'=> '2024-11-10 17:29:06',
@@ -24,7 +24,31 @@ class TrainsTableSeeder extends Seeder
                 'Number_carriages'=>4,
                 'in_time'=>1,
                 'Deleted'=>0
-            ]
+            ],
+            [
+
+                'agency'=>'Italia',
+                'Departure_station'=>'Milano',
+                'Arrival_station'=>'Bari',
+                'Departure_time'=> '2024-12-10 15:13:09',
+                'Arrival_time'=>'2024-12-10 19:11:02',
+                'Train_code'=>133,
+                'Number_carriages'=>4,
+                'in_time'=>1,
+                'Deleted'=>0
+            ],
+            [
+
+                'agency'=>'Italia',
+                'Departure_station'=>'Matera',
+                'Arrival_station'=>'Sicilia',
+                'Departure_time'=> '2024-11-10 7:12:03',
+                'Arrival_time'=>'2024-11-10 9:11:02',
+                'Train_code'=>134,
+                'Number_carriages'=>5,
+                'in_time'=>1,
+                'Deleted'=>0
+            ],
 
         ];
 
